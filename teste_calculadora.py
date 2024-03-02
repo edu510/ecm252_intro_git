aa8f6b748cb60c8ef346922c153b82f19db88f6c
Eduardo <20.01913-0@maua.br>
2024-03-02 12:19:29 -0300

    test(intro-git): testa subtracao

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -2,7 +2,11 @@ import calculadora
 def main():
     a = 2
     b = 3
+
     soma = calculadora.soma(a, b)
     print(f'{a} + {b} = {soma}')
+    
+    subtracao = calculadora.subtrai(a, b)
+    print(f'{a} - {b} = {subtracao}')
 
 main()
